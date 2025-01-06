035adcdfd20307ba80ef48ce55364317862d465f
Hinyi <mrmck16@gmail.com>
2025-01-06 19:17:15 +0100

    Init

---
+++ Users/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Users.Persistence;
+
+namespace Users;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddIdentity(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<UserDbContext>(options =>
+            options
+                .UseNpgsql(configuration.GetConnectionString("UserDb")));
+        
+        return services;
+    }
+}
---
+++ Users/Persistence/UserDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Users.Persistence;
+
+public class UserDbContext : DbContext
+{ 
+    public UserDbContext(DbContextOptions options) : base(options)
+    {
+    }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(UserDbContext).Assembly);
+    }
+}
