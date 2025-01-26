860bf81b76e7ea5cae735eb817b8792e52201186
Hinyi <mrmck16@gmail.com>
2025-01-26 15:27:04 +0100

    Fixed exceptions

--- Identity.API/Program.cs
+++ Identity.API/Program.cs
@@ -6,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.Users(builder.Configuration);
 builder.Services.AddControllers();
+builder.Services.AddShared();
 
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddEndpointsApiExplorer();
