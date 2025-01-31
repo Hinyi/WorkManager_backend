3aa194b19f37b07a4ab04fa48bad878b778bff39
Hinyi <mrmck16@gmail.com>
2025-01-31 23:50:17 +0100

    Fixed DTO

--- IdentityService/Aplication/User/DTOs/UserDTO.cs
+++ IdentityService/Aplication/User/DTOs/UserDTO.cs
@@ -2,7 +2,7 @@ namespace IdentityService.Aplication.User.DTOs;
 
 public class UserDTO
 {
-    public int Id { get; set; }
+    public string Id { get; set; }
     public string? UserName { get; set; } 
     public string? Email { get; set; }
 }
