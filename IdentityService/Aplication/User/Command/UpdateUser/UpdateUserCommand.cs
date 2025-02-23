62f4bb1bf68771c0d61aa6cc95399a3765287362
Hinyi <mrmck16@gmail.com>
2025-02-23 21:36:35 +0100

    Updated request for update username

--- IdentityService/Aplication/User/Command/UpdateUser/UpdateUserCommand.cs
+++ IdentityService/Aplication/User/Command/UpdateUser/UpdateUserCommand.cs
@@ -1,3 +1,6 @@
+using IdentityService.Aplication.User.DTOs;
+using MediatR;
+
 namespace IdentityService.Aplication.User.Command.UpdateUser;
 
-public record UpdateUserCommand();
+public record UpdateUserCommand(string userId, string username) : IRequest<UserDTO>;
--- IdentityService/Aplication/User/Command/UpdateUser/UpdateUserCommandHandler.cs
+++ IdentityService/Aplication/User/Command/UpdateUser/UpdateUserCommandHandler.cs
@@ -1,6 +1,42 @@
+using IdentityService.Aplication.User.DTOs;
+using IdentityService.Exceptions;
+using IdentityService.Interface;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
 namespace IdentityService.Aplication.User.Command.UpdateUser;
 
-public class UpdateUserCommandHandler
+public class UpdateUserCommandHandler : IRequestHandler<UpdateUserCommand, UserDTO>
 {
-    
+    private readonly ILogger<UpdateUserCommandHandler> _logger;
+    private readonly IUserRepository _userRepository;
+    private readonly IUserMapper _userMapper;
+
+    public UpdateUserCommandHandler(ILogger<UpdateUserCommandHandler> logger, IUserRepository userRepository, IUserMapper userMapper)
+    {
+        _logger = logger;
+        _userRepository = userRepository;
+        _userMapper = userMapper;
+    }
+
+
+    public async Task<UserDTO> Handle(UpdateUserCommand request, CancellationToken cancellationToken)
+    {
+        var user = await _userRepository.GetUserById(request.userId, cancellationToken);
+
+        if (user == null)
+        {
+            _logger.LogError("User not found");
+            throw new UserNotFound(nameof(User), request.userId);
+        }
+
+        user.UserName = request.username;
+        
+        
+        await _userRepository.UpdateUser(user);
+
+        var userResponse = _userMapper.MapToUserDto(user);
+        
+        return userResponse;
+    }
 }
